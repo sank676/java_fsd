efec7bac08fa70f1435818fca520a1f51298f50c
sank676 <72061055+sank676@users.noreply.github.com>
2023-03-14 09:42:51 +0530

    Update Textile.java

--- Textile.java
+++ Textile.java
@@ -1,3 +1,8 @@
+/*consider there is a textile industry. During the discussion about profit and loss, they wanted to design an application.
+if given a date it should display total number of shirt,pant,saree sold and the total profit from each.
+implement java 8 code with TDD approach*/
+
+
 package ustbatchno3.junit5testcases;
 
 import java.time.LocalDate;
